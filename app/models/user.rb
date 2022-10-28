6c8516250b70a6245d9dbb0fa777cc4bf8a50517
tylergubser <tylergubser2200@gmail.com>
2022-10-27 19:17:16 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true
     validates :email, presence: true, uniqueness: true
+    validates :age, presence: true
+
 end
